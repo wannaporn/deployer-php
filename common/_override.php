a436d79549569b3660674914fee85030814cbaef
liverbool <nukboon@gmail.com>
2018-02-08 03:04:50 +0700

    add deploy:copy_files

--- common/_override.php
+++ common/_override.php
@@ -19,3 +19,17 @@ task('deploy:assets', function () {
     // BUGFIX: liip imagine need image folder
     run("mkdir -p {{deploy_path}}/shared/web/media/image");
 })->desc('Normalize asset timestamps');
+
+/**
+ * Adding not override!
+ * Copy directories. Useful for vendors directories
+ */
+task('deploy:copy_files', function () {
+    $files = get('copy_files');
+    foreach ($files as $file) {
+        // Delete file if exists.
+        run("if [ -f $(echo {{release_path}}/$file) ]; then rm -rf {{release_path}}/$file; fi");
+        // Copy file.
+        run("if [ -f $(echo {{deploy_path}}/current/$file) ]; then cp -rpf {{deploy_path}}/current/$file {{release_path}}/$file; fi");
+    }
+})->desc('Copy files')->setPrivate();
